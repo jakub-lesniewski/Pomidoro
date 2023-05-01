298d3654a626d068a2a7c45dbb09bf2a81ca1c48
ayroh <jakubjanlesniewski@gmail.com>
2023-05-01 18:53:50 +0200

    initial layout

--- script.js
+++ script.js
@@ -4,20 +4,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.querySelector(".start");
   const pauseBtn = document.querySelector(".pause");
   const resetBtn = document.querySelector(".reset");
+  const closeBtn = document.querySelector(".close");
+  const settingsBtn = document.querySelector(".settings");
   const pomodoros = document.querySelectorAll(".pomodoro-container .pomodoro");
   const secondsLabel = document.querySelector(".seconds");
   const minutesLabel = document.querySelector(".minutes");
+  const settingsContainer = document.querySelector(".container-left");
 
   const audio = new Audio("ding.mp3");
 
   let seconds = 0;
   let minutes = 0;
+
+  let workTime = 25;
+  let shortBreakTime = 5;
+  let longBreakTime = 20;
+
   let currentPomodoro = 0;
+
   let interval;
 
   let isPaused = false;
-  let isShortBreak = false;
-  let isLongBreak = false;
+  let isBreak = false;
+
+  // Pomodoro functionality
+  const resetTimer = () => {
+    seconds = 0;
+    minutes = 0;
+  };
 
   const displayButtons = () => {
     pauseBtn.classList.remove("hidden");
@@ -25,67 +39,46 @@ document.addEventListener("DOMContentLoaded", () => {
     startBtn.classList.add("hidden");
   };
 
-  const shortBreak = () => {
+  const takeBreak = () => {
     audio.play();
+    pomodoros[currentPomodoro].classList.add("pomodoro-active");
     currentPomodoro++;
-    isShortBreak = true;
 
-    document.title = "Take a break! 🍅";
+    if (currentPomodoro === pomodoros.length) {
+      isBreak = true;
+      document.title = "🍅 Long break";
+      resetTimer();
 
-    seconds = 0;
-    minutes = 0;
-  };
-
-  const longBreak = () => {
-    audio.play();
-    isLongBreak = true;
-
-    pomodoros.forEach(function (element) {
-      element.classList.remove("pomodoro-active");
-    });
-
-    document.title = "Take a long break! 🍅";
-
-    seconds = 0;
-    minutes = 0;
-    currentPomodoro = 0;
+      setTimeout(() => {
+        pomodoros.forEach((pomodoro) => {
+          pomodoro.classList.remove("pomodoro-active");
+        });
+        currentPomodoro = 0;
+        resetTimer();
+        isBreak = false;
+        document.title = "🍅 Pomodoro";
+        audio.play();
+      }, longBreakTime * 1000 * 60);
+    } else {
+      isBreak = true;
+      document.title = "🍅 Short break";
+      resetTimer();
+      setTimeout(() => {
+        pomodoros.forEach((pomodoro) => {});
+        isBreak = false;
+        resetTimer();
+        document.title = "🍅 Pomodoro";
+        audio.play();
+      }, shortBreakTime * 1000 * 60);
+    }
   };
 
   const updateDisplay = () => {
     secondsLabel.textContent = seconds < 10 ? `0${seconds}` : seconds;
     minutesLabel.textContent = minutes < 10 ? `0${minutes}` : minutes;
 
-    if (
-      seconds === 2 &&
-      isShortBreak === false &&
-      currentPomodoro <= pomodoros.length
-    ) {
-      pomodoros[currentPomodoro].classList.add("pomodoro-active");
-      shortBreak();
-    }
-
-    if (isShortBreak && seconds === 1) {
-      isShortBreak = false;
-      audio.play();
-      document.title = "Pomodoro Timer 🍅";
-      seconds = 0;
-      minutes = 0;
-    }
-
-    if (
-      currentPomodoro === pomodoros.length &&
-      isLongBreak === false &&
-      seconds === 5
-    ) {
-      longBreak();
-    }
-
-    if (isLongBreak && seconds === 5) {
-      isLongBreak = false;
-      audio.play();
-      document.title = "Pomodoro Timer 🍅";
-      seconds = 0;
-      minutes = 0;
+    if (minutes === workTime && isBreak === false) {
+      takeBreak();
     }
   };
 
@@ -118,5 +111,16 @@ document.addEventListener("DOMContentLoaded", () => {
   pauseBtn.addEventListener("click", toggleWatch);
   resetBtn.addEventListener("click", () => {
     location.reload();
+  });
+
+  // Settings
+  settingsBtn.addEventListener("click", () => {
+    settingsContainer.classList.toggle("hidden");
+    settingsBtn.classList.toggle("hidden");
+  });
+
+  closeBtn.addEventListener("click", () => {
+    settingsContainer.classList.toggle("hidden");
+    settingsBtn.classList.toggle("hidden");
   });
 });
