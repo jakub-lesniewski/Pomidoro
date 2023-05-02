100b02b9bd2d67d1ac7d33b5b607bcd419802214
ayroh <jakubjanlesniewski@gmail.com>
2023-05-02 12:17:32 +0200

    refactoring logic and settings into two different files to improve readability

--- pomodoro.js
+++ pomodoro.js
@@ -4,39 +4,24 @@ document.addEventListener("DOMContentLoaded", () => {
   const startBtn = document.querySelector(".start");
   const pauseBtn = document.querySelector(".pause");
   const resetBtn = document.querySelector(".reset");
-  const closeBtn = document.querySelector(".close");
-  const settingsBtn = document.querySelector(".settings");
-
   const pomodoros = document.querySelectorAll(".pomodoro-container .pomodoro");
-
   const secondsLabel = document.querySelector(".seconds");
   const minutesLabel = document.querySelector(".minutes");
-
-  const settingsContainer = document.querySelector(".container-left");
-  const workDurationInput = document.querySelector(".work-duration");
-  const shortBreakDurationInput = document.querySelector(
-    ".short-break-duration"
-  );
-  const longBreakDurationInput = document.querySelector(".long-break-duration");
-  const settingsForm = document.querySelector(".settings-form");
 
   const audio = new Audio("ding.mp3");
 
   let seconds = 0;
   let minutes = 0;
+  let currentPomodoro = 0;
 
   let workTime = 25;
   let shortBreakTime = 5;
   let longBreakTime = 20;
 
-  let currentPomodoro = 0;
-
   let interval;
-
   let isPaused = false;
   let isBreak = false;
 
-  // Pomodoro functionality
   const resetTimer = () => {
     seconds = 0;
     minutes = 0;
@@ -120,60 +105,5 @@ document.addEventListener("DOMContentLoaded", () => {
   pauseBtn.addEventListener("click", toggleWatch);
   resetBtn.addEventListener("click", () => {
     location.reload();
-  });
-
-  // Settings
-  const toggleSettings = () => {
-    settingsContainer.classList.toggle("hidden");
-    settingsBtn.classList.toggle("hidden");
-  };
-
-  settingsBtn.addEventListener("click", toggleSettings);
-
-  closeBtn.addEventListener("click", toggleSettings);
-
-  // Retrieve the settings from localStorage
-  const workTimeLS = localStorage.getItem("workTime");
-  const shortBreakTimeLS = localStorage.getItem("shortBreakTime");
-  const longBreakTimeLS = localStorage.getItem("longBreakTime");
-
-  // local storage handling
-  function updateLocalStorage() {
-    // Store the settings in localStorage
-    localStorage.setItem("workTime", workTime);
-    localStorage.setItem("shortBreakTime", shortBreakTime);
-    localStorage.setItem("longBreakTime", longBreakTime);
-
-    // Display the updated values in HTML
-    workDurationInput.value = workTime;
-    shortBreakDurationInput.value = shortBreakTime;
-    longBreakDurationInput.value = longBreakTime;
-  }
-
-  // Retrieve the settings from localStorage on page load
-  if (localStorage.getItem("workTime")) {
-    workTime = parseInt(localStorage.getItem("workTime"));
-    workDurationInput.value = workTime;
-  }
-
-  if (localStorage.getItem("shortBreakTime")) {
-    shortBreakTime = parseInt(localStorage.getItem("shortBreakTime"));
-    shortBreakDurationInput.value = shortBreakTime;
-  }
-
-  if (localStorage.getItem("longBreakTime")) {
-    longBreakTime = parseInt(localStorage.getItem("longBreakTime"));
-    longBreakDurationInput.value = longBreakTime;
-  }
-
-  // Update the settings in localStorage on submit
-  settingsForm.addEventListener("submit", (event) => {
-    event.preventDefault();
-
-    workTime = parseInt(workDurationInput.value);
-    shortBreakTime = parseInt(shortBreakDurationInput.value);
-    longBreakTime = parseInt(longBreakDurationInput.value);
-
-    updateLocalStorage();
   });
 });
---
+++ settings.js
@@ -0,0 +1,66 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const settingsBtn = document.querySelector(".settings");
+  const closeBtn = document.querySelector(".close");
+  const settingsContainer = document.querySelector(".container-left");
+  const workDurationInput = document.querySelector(".work-duration");
+  const shortBreakDurationInput = document.querySelector(
+    ".short-break-duration"
+  );
+  const longBreakDurationInput = document.querySelector(".long-break-duration");
+  const settingsForm = document.querySelector(".settings-form");
+
+  let workTime = 25;
+  let shortBreakTime = 5;
+  let longBreakTime = 20;
+
+  // Toggle settings visibility
+  const toggleSettings = () => {
+    settingsContainer.classList.toggle("hidden");
+    settingsBtn.classList.toggle("hidden");
+  };
+
+  settingsBtn.addEventListener("click", toggleSettings);
+  closeBtn.addEventListener("click", toggleSettings);
+
+  // Update settings values and localStorage
+  const updateLocalStorage = () => {
+    localStorage.setItem("workTime", workTime);
+    localStorage.setItem("shortBreakTime", shortBreakTime);
+    localStorage.setItem("longBreakTime", longBreakTime);
+
+    workDurationInput.value = workTime;
+    shortBreakDurationInput.value = shortBreakTime;
+    longBreakDurationInput.value = longBreakTime;
+  };
+
+  const updateSettings = () => {
+    workTime = parseInt(workDurationInput.value);
+    shortBreakTime = parseInt(shortBreakDurationInput.value);
+    longBreakTime = parseInt(longBreakDurationInput.value);
+
+    updateLocalStorage();
+  };
+
+  settingsForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+    updateSettings();
+  });
+
+  // Retrieve settings from localStorage
+  if (localStorage.getItem("workTime")) {
+    workTime = parseInt(localStorage.getItem("workTime"));
+    workDurationInput.value = workTime;
+  }
+
+  if (localStorage.getItem("shortBreakTime")) {
+    shortBreakTime = parseInt(localStorage.getItem("shortBreakTime"));
+    shortBreakDurationInput.value = shortBreakTime;
+  }
+
+  if (localStorage.getItem("longBreakTime")) {
+    longBreakTime = parseInt(localStorage.getItem("longBreakTime"));
+    longBreakDurationInput.value = longBreakTime;
+  }
+});
